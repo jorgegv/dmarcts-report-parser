22516afdb9830cf505c09af23fb8cd403dfe0db1
Jorge Gonzalez <jorgegv@icij.org>
2025-05-07 18:09:55 +0200

    dmarcts-report-aggregator: add target date options, db table check and some debug logging

--- dmarcts-report-aggregator.pl
+++ dmarcts-report-aggregator.pl
@@ -27,10 +27,47 @@
 use Modern::Perl;
 use DBI;
 use File::Basename;
+use Getopt::Std;
+use POSIX qw(strftime);
 
 my $scriptname = basename( $0 );
 
 our ( $dbtype, $dbname, $dbuser, $dbpass, $dbhost, $dbport, $db_tx_support, $debug );
+
+sub show_usage {
+    print <<EOF_USAGE
+usage: $scriptname [-t <target_date>] [-b <days_before_today>]
+    <target_date> in format 'YYYY-MM-DD'
+EOF_USAGE
+;
+}
+
+# parse cli options
+# -t <date>: aggregate data for a given date in format YYYY-MM-DD
+# -b <n>: aggregate data for today -n days (n=1 -> yesterday)
+our ( $opt_t, $opt_b, $opt_h );
+getopts( "t:b:h" );
+defined( $opt_h ) and do {
+  show_usage();
+  exit;
+};
+
+my $target_date = $opt_t || undef;
+my $days_before = $opt_b || undef;
+defined( $opt_t ) or defined( $opt_b ) or do {
+  show_usage();
+  exit;
+};
+
+if ( defined( $target_date ) ) {
+    ( $target_date =~ m/^\d{4}\-\d{2}\-\d{2}$/ ) or
+        die "$scriptname: <target_date> for -t must be in format YYYY-MM-DD\n";
+} else {
+    ( defined( $days_before ) and ( $days_before =~ /^\d+$/ ) ) or
+        die "$scriptname: <n> for -b must be an integer\n";
+    $target_date = strftime( '%Y-%m-%d', localtime() - $days_before * 24 * 3600 );
+}
+printf( "Target date: %s\n", $target_date ) if $debug;
 
 my $conf_file = 'dmarcts-report-aggregator.conf';
 
@@ -51,8 +88,7 @@ if ( substr($conf_file, 0, 1) ne '/'  and substr($conf_file, 0, 1) ne '.') {
 my $conf_return = do $conf_file;
 die "$scriptname: couldn't parse $conf_file: $@" if $@;
 die "$scriptname: couldn't do $conf_file: $!"    unless defined $conf_return;
-
-
+print "Configuration loaded\n" if $debug;
 
 # Setup connection to database server.
 $db_tx_support  = 1;
@@ -68,17 +104,17 @@ or die "$scriptname: Cannot connect to database\n";
 if ($db_tx_support) {
         $dbh->{AutoCommit} = 0;
 }
-checkDatabase($dbh);
+print "Database connection established\n" if $debug;
 
-sub checkDatabase {
-    my $dbh = shift;
-    # check that table 'metric' exists
-    my $query_handle = $dbh->prepare("DESC metric");
-    $query_handle->{RaiseError} = 0;
-    $query_handle->{PrintError} = 0;
-    if ( not $query_handle->execute() ) {
-        die "$scriptname: table 'metric' not found, please run dmarcts-report-parser.pl at least once\n";
-    };
-}
+# check that table 'metric' exists
+my $query_handle = $dbh->prepare("DESC metric");
+$query_handle->{RaiseError} = 0;
+$query_handle->{PrintError} = 0;
+if ( not $query_handle->execute() ) {
+    die "$scriptname: table 'metric' not found, please run dmarcts-report-parser.pl at least once\n";
+};
+print "Table 'metric' exists\n" if $debug;
 
+# end of party
 $dbh->disconnect();
+printf( "Target date: %s\n", $target_date ) if $debug;
